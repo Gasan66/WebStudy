9174d191d8daa1432186c8f39ddd0a091d0086a2
gasan <abduragimov.ga@gmail.com>
2017-12-20 16:30:13 +0500

    1

--- Init.sh
+++ Init.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
-sudo apt-get update
-sudo apt-get install nginx
-sudo /etc/init.d/nginx start
-sudo apt-get install curl
-mkdir web
-mkdir web/public
-mkdir web/public/img
-mkdir web/public/css
-mkdir web/public/js
-mkdir web/uploads
-mkdir web/etc
-mv -f WebStudy/nginx.conf web/etc/
+sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
